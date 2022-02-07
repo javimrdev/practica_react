8449d569ce8f236d2cd8b4a8fb7fa189f73326c6
Javier M <javimrdev@gmail.com>
2022-02-07 11:52:58 +0100

    added form css class

--- src/pods/company-employee-list/components/searcher.component.tsx
+++ src/pods/company-employee-list/components/searcher.component.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import { css } from "@emotion/react";
 
 interface Props {
     onSubmitHandler: (value: string) => void;
@@ -12,9 +13,14 @@ export const SearcherComponent: React.FC<Props> = (props) => {
     const { onSubmitHandler, defaultValue } = props;
     const [value, setValue] = React.useState<string>(defaultValue);
 
+    const formClass = css`
+        display: flex;
+        gap: 10px 10px;
+    `;
+
     return (
-        <form onSubmit={() => onSubmitHandler(value)}>
-            <TextField id="outlined-basic" variant="outlined" defaultValue={value} onKeyUp={(event: any) => setValue(event.target.value)} />
+        <form css={formClass} onSubmit={() => onSubmitHandler(value)}>
+            <TextField id="outlined-basic" variant="outlined" size="small" defaultValue={value} onKeyUp={(event: any) => setValue(event.target.value)} />
             <Button type="submit" variant="contained">Search</Button>
         </form>
     )
